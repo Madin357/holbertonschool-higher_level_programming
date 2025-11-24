bf4fff4bff50da3156d1356eb295f982964d7dae
madin357 <madin.qahramanov@gmail.com>
2025-11-24 13:55:12 +0000

    rectangle

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-"""
-This module defines an empty Rectangle class.
-"""
-
+"""This module defines an empty Rectangle class."""
 class Rectangle:
     """Represents an empty rectangle."""
     pass
