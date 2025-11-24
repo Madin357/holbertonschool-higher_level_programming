fb10ad3e2b3409e43da9769a5b59c9a82c69cdfc
madin357 <madin.qahramanov@gmail.com>
2025-11-24 14:38:50 +0000

    sameclass

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Module 2-is_same_class
+Defines a function that checks if an object is exactly an instance
+of a specified class.
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Return True if obj is exactly an instance of a_class,
+    otherwise return False.
+
+    Args:
+        obj: The object to check.
+        a_class: The class to compare against.
+
+    Returns:
+        bool: True if obj is exactly an instance of a_class.
+    """
+    return type(obj) is a_class
