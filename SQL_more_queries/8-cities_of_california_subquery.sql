3b2f10a73f1bb01b0c4ce983ede6870cf3569dfc
madin357 <madin.qahramanov@gmail.com>
2025-11-26 13:21:43 +0000

    cities of cali sub

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,7 +1,13 @@
+-- List all cities of California
+-- California's ID is not known, so we find it with a subquery
+-- No JOIN keyword is used
+-- Results sorted by cities.id
+
 SELECT cities.id, cities.name
 FROM cities
 WHERE cities.state_id = (
-    SELECT id FROM states WHERE name = 'California' LIMIT 1
+    -- Select the id of the state named "California"
+    SELECT id FROM states WHERE name = 'California'
 )
 ORDER BY cities.id ASC;
 
