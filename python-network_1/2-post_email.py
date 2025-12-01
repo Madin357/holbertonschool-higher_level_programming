aa74c1ae0b13f827e2edc76bd4c44f82cb0edcf8
Madin357 <madin.qahramanov@gmail.com>
2025-12-01 15:31:24 +0000

    post email

---
+++ python-network_1/2-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Sends a POST request to a given URL with an email parameter
+and displays the response body (decoded in UTF-8).
+"""
+
+import sys
+import urllib.parse
+import urllib.request
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    data = urllib.parse.urlencode({"email": email}).encode("utf-8")
+
+    with urllib.request.urlopen(url, data=data) as response:
+        body = response.read().decode("utf-8")
+        print(body)
